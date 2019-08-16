7c93588a4a0736029f6813bf37f391c3799e60a7
maxime.perrimond <maxime.perrimond@zenport.io>
2019-08-16 11:32:26 +0900

    update metadata and create deploy

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import * as core from '@actions/core';
+import * as now from 'now-client';
 
 async function run() {
   try {
-    const myInput = core.getInput('myInput');
-    core.debug(`Hello ${myInput}`);
+    const token = core.getInput('zeit-token');
+    const folder = core.getInput('folder') || '';
+
+    for await (const event of now.createDeployment(`${__dirname}/${folder}`, { token })) {
+      if (event.type === 'ready') {
+        console.log(event.payload);
+        break;
+      }
+    }
+
   } catch (error) {
     core.setFailed(error.message);
   }
