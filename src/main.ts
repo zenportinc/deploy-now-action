206a8f08d814cdad7350d3d08fda25c3cb4520aa
maxime.perrimond <maxime.perrimond@zenport.io>
2019-08-16 14:09:12 +0900

    change path and post comment

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,44 @@
+import * as fs from 'fs';
+import * as github from '@actions/github';
 import * as core from '@actions/core';
 import * as now from 'now-client';
 
 async function run() {
   try {
-    const token = core.getInput('zeit-token');
-    const folder = core.getInput('folder') || '';
+    const zeitToken = core.getInput('zeit-token');
+    const githubToken = core.getInput('repo-token');
+    const repoDir = core.getInput('repo-dir');
+    const buildDir = core.getInput('build-dir') || '';
 
-    for await (const event of now.createDeployment(`${__dirname}/${folder}`, { token })) {
-      if (event.type === 'ready') {
-        console.log(event.payload);
-        break;
-      }
-    }
+    const nowJson = fs.readFileSync('now.json', 'utf8');
 
+    const deployment = await deploy(`${repoDir}/${buildDir}`, zeitToken, JSON.parse(nowJson));
+
+    await postComment(deployment, githubToken);
   } catch (error) {
     core.setFailed(error.message);
   }
+}
+
+async function deploy(path: string, token: string, options: object) {
+  for await (const event of now.createDeployment(path, {
+    token,
+    ...options
+  })) {
+    if (event.type === 'ready') {
+      console.log(event.payload);
+      return event.payload;
+    }
+  }
+}
+
+async function postComment(body: string, token: string) {
+  const octokit = new github.GitHub(token);
+
+  await octokit.issues.createComment({
+    ...github.context.issue,
+    body,
+  })
 }
 
 run();
